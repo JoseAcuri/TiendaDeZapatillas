e7b2c4d98834bd1e3dbb04215d2a474bc1e30316
Jose Curi <josealejandrocuri@gmail.com>
2023-10-18 20:29:52 -0300

    formulario

--- js/form.js
+++ js/form.js
@@ -1,13 +1,11 @@
 const checkoutBtn = document.getElementById("checkout-btn");
 
-// Agrega un controlador de eventos para el botón "Proceed to Checkout"
+
 checkoutBtn.addEventListener("click", function (event) {
-    event.preventDefault(); // Evita que el formulario se envíe
+    event.preventDefault(); 
 
-    // Aquí puedes agregar la lógica para finalizar la compra
-    // Puedes utilizar SweetAlert2 u otra forma de confirmación
-    // antes de realizar la compra.
-
+   
+    
     Swal.fire({
         title: '¿Desea finalizar la compra?',
         text: 'Esto procesará su pedido y realizará la compra.',
@@ -17,13 +15,13 @@ checkoutBtn.addEventListener("click", function (event) {
         cancelButtonText: '<i class="fas fa-times"></i> Cancelar'
     }).then((result) => {
         if (result.isConfirmed) {
-            // Lógica para finalizar la compra
+            
 
-            // Puedes limpiar el formulario estableciendo los valores de entrada en blanco
+           
             const form = document.getElementById("myForm");
             form.reset();
 
-            // Mostrar el mensaje de compra exitosa de la misma manera que el modal del carrito
+            
             modalContainer.innerHTML = " ";
             modalContainer.style.display = "flex";
             const modalHeader = document.createElement("div");
@@ -44,7 +42,7 @@ checkoutBtn.addEventListener("click", function (event) {
                 modalContainer.style.display = "none"
             });
 
-            // Puedes personalizar el mensaje de compra exitosa aquí
+            
             const mensajeCompraExitosa = document.createElement("p");
             mensajeCompraExitosa.innerHTML = "Gracias por su compra. Su pedido ha sido procesado con éxito.";
             mensajeCompraExitosa.className = "mensaje-compra-exitosa";
